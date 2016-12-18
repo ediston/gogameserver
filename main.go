76b004a1ad8a4e0d7a0bd733db037a3e1d731796
ediston <vivek4dhiman@gmail.com>
2016-12-18 15:06:24 +0000

    added http handlers in main

--- main.go
+++ main.go
@@ -4,11 +4,19 @@ import (
     "fmt"
     "log"
     "net/http"
+    "os"
+
+    httpHandler "gogameserver/httpreqhandler"
 )
 
 
 var (
-    AppLogFileName      = "/app/log/app.log"
+    AppLogFileName  = os.Getenv("APP_LOG_FILE_PATH")
+    GET_TOP_PLAYERS_URL = os.Getenv("GET_TOP_PLAYERS_URL")
+    UPDATE_PLAYER_DATA_URL = os.Getenv("UPDATE_PLAYER_DATA_URL")
+    GET_PLAYER_RANK_URL = os.Getenv("GET_PLAYER_RANK_URL")
+    UPDATE_PLAYER_SCORE_URL = os.Getenv("UPDATE_PLAYER_SCORE_URL")  
+    PING_URL = os.Getenv("PING_URL")  
 )
 
 func cleaner(){
@@ -17,16 +25,25 @@ func cleaner(){
     }
 }
 
-func echoString(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "hello")
-}
-
 func main() {
     go cleaner()
     InitLogging(AppLogFileName)
-  
-    http.HandleFunc("/", echoString)
-    log.Fatal(http.ListenAndServe(":8081", nil))
+    
+    defineHandlers()
+
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handlePing(w http.ResponseWriter, req *http.Request) {
+    fmt.Fprintf(w, "Gogameserver")
+}
+
+func defineHandlers(){
+    http.HandleFun(PING_URL,                 handlePing)
+    http.HandleFun(GET_TOP_PLAYERS_URL,     httpHandler.HandleGetTopScorers)
+    http.HandleFun(UPDATE_PLAYER_DATA_URL,     httpHandler.HandleUpdatePlayerData)
+    http.HandleFun(GET_PLAYER_RANK_URL,     httpHandler.HandleGetPlayerRank)
+    http.HandleFun(UPDATE_PLAYER_SCORE_URL,     httpHandler.HandleUpdatePlayerScore )
 }
 
 func InitLogging(logFileName string) {
@@ -34,7 +51,7 @@ func InitLogging(logFileName string) {
     if err != nil {
         fmt.Println(err)
     }
-    
+
     log.SetFlags(0)
     log.SetOutput(newLog)
 }
