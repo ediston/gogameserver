853084216e40cbb39bf130867b2de632da12c8eb
ediston <vivek4dhiman@gmail.com>
2016-12-26 12:30:08 +0000

    all data type member are string

--- datatypes/datatypes.go
+++ datatypes/datatypes.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 )
 
+/*
+-- IT string	//idType are: D=device, FB: Facebook, Twitter
+*/
 type PlayerData struct {
 	I string  	//id   
 	IT string	//idType
@@ -12,10 +15,10 @@ type PlayerData struct {
 	L int64	`json:",string"`    //lastPlayed string
 	NP int64 `json:",string"`	//numOfTimesPlayed int64
 	A float64	 `json:",string"`	//allTimeHighScore int64
-	O []string	//oldIds []string
-	OT []string	//oldIdtypes []string
-	D [] string //devices [] string
-	OS []string	//os []string
+	O string	//oldIds string
+	OT string  //oldIdtypes string
+	D  string // devices   string
+	OS string	//os  string
 }
 
 func New(	id string, 
@@ -25,39 +28,39 @@ func New(	id string,
 			lastPlayed int64,
 			numOfTimesPlayed int64,
 			allTimeHighScore float64,
-			oldIds []string,
-			oldIdtypes []string,
-			devices [] string,
-			os []string	) (PlayerData) {
+			oldIds string,
+			oldIdtypes string,
+			devices string,
+			os string	) (PlayerData) {
 
     return PlayerData{
         I  : id, 
-		IT  : idType,
+		IT : idType,
 		N  : name,
 		J  : joinDate,
 		L  : lastPlayed,
-		NP  : numOfTimesPlayed,
-		A   : allTimeHighScore,
+		NP : numOfTimesPlayed,
+		A  : allTimeHighScore,
 		O  : oldIds,
-		OT  : oldIdtypes,
-		D   : devices,
-		OS  : os,
+		OT : oldIdtypes,
+		D  : devices,
+		OS : os,
     }
 }
 
 func NewWithId(id string) (PlayerData){
     return PlayerData{
         I  : id, 
-		IT  : "",
+		IT : "",
 		N  : "",
 		J  : 0,
 		L  : 0,
-		NP  : 0,
-		A   : 0,
-		O  : []string{},
-		OT  : []string{},
-		D   : []string{},
-		OS  : []string{},
+		NP : 0,
+		A  : 0,
+		O  : "",
+		OT : "",
+		D  : "",
+		OS : "",
     }
 }
 
